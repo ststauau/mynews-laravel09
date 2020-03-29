ee389cf3a192cc11e330e52ab014ef9161244dd9
”asano” <ststauau02@leo.nifty.jp>
2020-03-29 14:04:35 +0000

    Laravle 09 訂正

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CREATE｜MyNews</title>
+    </head>
+    <body>
+        <h1>CREATE</h1>
+        
+        <p>管理者を<span style="font:#FF0000">作成するページ</span>のview</p>
+        
+        
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>EDIT｜MyNews</title>
+    </head>
+    <body>
+        <h1>CREATE</h1>
+        
+        <p>管理者を<span style="font:#00FF00">編集する</span>ページのview</p>
+        
+        
+    </body>
+</html>
